4cd9675113bf07d092d5d6019d2df2f401b19345
alfclul <69283001+alfclul@users.noreply.github.com>
2023-05-12 15:17:04 -0400

    Update Employe.java

--- src/payables/Employe.java
+++ src/payables/Employe.java
@@ -1,18 +1,35 @@
 package payables;
 
+/**
+ * Classe abstraite représentant la base commune des différentes sortes d'employés.
+ */
 public abstract class Employe extends Payable {
-	private final String nomComplet, numeroAssuranceSociale;
+	private final String nomComplet;
+	private final String numeroAssuranceSociale;
 
+	/**
+	 * Construit un nouvel Employé.
+	 * @param id Numéro d'identification unique du Payable.
+	 * @param nom Prénom et nom de l'employé.
+	 * @param nas Numéro d'assurance sociale de l'employé.
+	 * @param memo Champ texte optionnel pour laisser un mémo.
+	 */
 	public Employe(int id, String nom, String nas, String memo) {
 		super(id, memo);
 		this.nomComplet = nom;
 		numeroAssuranceSociale = nas;
 	}
 
+	/**
+	 * @return le nom complet (prénom et nom) de l'employé.
+	 */
 	public String getNomComplet() {
 		return nomComplet;
 	}
 
+	/**
+	 * @return le numéro d'assurance sociale de l'employé.
+	 */
 	public String getNumeroAssuranceSociale() {
 		return numeroAssuranceSociale;
 	}
